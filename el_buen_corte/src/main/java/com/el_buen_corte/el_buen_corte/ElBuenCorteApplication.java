2cc185c786c5a1bab5f56eac29c9953093b875bd
jaimeGLT <jaimeguzmanlt@gmail.com>
2025-11-13 07:17:01 -0400

    last push

--- el_buen_corte/src/main/java/com/el_buen_corte/el_buen_corte/ElBuenCorteApplication.java
+++ el_buen_corte/src/main/java/com/el_buen_corte/el_buen_corte/ElBuenCorteApplication.java
@@ -3,7 +3,7 @@ package com.el_buen_corte.el_buen_corte;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.el_buen_corte.el_buen_corte")
 public class ElBuenCorteApplication {
 
 	public static void main(String[] args) {
