c51e55b2d19af14468baf111c002620bc9e30519
jaimeGLT <jaimeguzmanlt@gmail.com>
2025-11-21 18:17:54 -0400

    application.dev added

--- el_buen_corte/src/main/java/com/el_buen_corte/el_buen_corte/config/SecurityConfiguration.java
+++ el_buen_corte/src/main/java/com/el_buen_corte/el_buen_corte/config/SecurityConfiguration.java
@@ -45,7 +45,7 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://el-buen-corte.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
