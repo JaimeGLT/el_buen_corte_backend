eff890c7a709a269b77259117415dc7b6a535ea1
jaimeGLT <jaimeguzmanlt@gmail.com>
2025-11-21 15:32:51 -0400

    docker

--- el_buen_corte/src/main/java/com/el_buen_corte/el_buen_corte/chat/GeminiController.java
+++ el_buen_corte/src/main/java/com/el_buen_corte/el_buen_corte/chat/GeminiController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.el_buen_corte.el_buen_corte.chat.dto.VentasHoyDTO;
-
 @RestController
 @RequestMapping("/api/v1/gemini")
 @CrossOrigin(origins = "*")
